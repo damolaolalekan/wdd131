166a3c752fa8b8f0e89e93bd67c63e8ce5708658
Olalekan Damola <olalekandamolaisaac@gmail.com>
2026-01-19 03:07:36 -0500

    To the moon

--- scripts/temples.js
+++ scripts/temples.js
@@ -15,6 +15,7 @@ if (lastModifiedSpan) {
 // Responsive Hamburger Menu
 const menuBtn = document.getElementById('menu');
 const nav = document.querySelector('header nav');
+const header = document.querySelector('header'); 
 
 if (menuBtn && nav) {
   menuBtn.addEventListener('click', (e) => {
@@ -22,12 +23,14 @@ if (menuBtn && nav) {
 
     nav.classList.toggle('show');
     menuBtn.classList.toggle('show');
+    header.classList.toggle('menu-open');
   });
 
   nav.querySelectorAll('a').forEach((link) => {
     link.addEventListener('click', () => {
       nav.classList.remove('show');
       menuBtn.classList.remove('show');
+      header.classList.remove('menu-open');
     });
   });
 }
