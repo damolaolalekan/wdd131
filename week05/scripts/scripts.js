b7d26bef30b6c761459fea2cd2732a69d5aa420d
Olalekan Damola <olalekandamolaisaac@gmail.com>
2026-02-10 02:33:31 -0500

    To the Mars

--- week05/scripts/scripts.js
+++ week05/scripts/scripts.js
@@ -29,10 +29,7 @@ products.forEach(product => {
   productSelect.appendChild(option);
 });
 
-let count = localStorage.getItem("reviewCount");
-  count = count ? Number(count) + 1 : 1;
-  localStorage.setItem("reviewCount", count);
-  console.log("Total reviews submitted:", count);
+
 
 
  
