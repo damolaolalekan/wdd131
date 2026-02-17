a1e05bd9df461a7a52b0610efb526b9178e527f5
Olalekan Damola <olalekandamolaisaac@gmail.com>
2026-02-17 02:27:42 -0500

    Send  to git

--- project/scripts/siteplan.js
+++ project/scripts/siteplan.js
@@ -28,3 +28,22 @@ window.addEventListener("resize", () => {
     header.classList.remove("menu-open");
   }
 });
+
+
+// Who We Serve Tab Switch
+const serveButtons = document.querySelectorAll(".serve-btn");
+const serveContent = document.querySelectorAll(".serve-content");
+
+serveButtons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    const target = btn.getAttribute("data-target");
+
+    // Remove active from all
+    serveButtons.forEach(b => b.classList.remove("active"));
+    serveContent.forEach(c => c.classList.remove("active"));
+
+    // Add active on clicked
+    btn.classList.add("active");
+    document.getElementById(target).classList.add("active");
+  });
+});
